6f6cd182045323c506e881c23b0ec9cce803e40a
pratik8696 <85107447+pratik8696@users.noreply.github.com>
2021-07-01 12:51:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var randomNumber1=Math.floor(Math.random() * 6)+1;
+var randomNumber2=Math.floor(Math.random() * 6)+1;
+var randomImage1="images/dice"+randomNumber1+".png";
+var randomImage2="images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[0].setAttribute("src",randomImage1);
+document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
+if (randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="🎉Player 1 Wins";}
+  if (randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Wins🎉";}
+    if (randomNumber1==randomNumber2){
+      document.querySelector("h1").innerHTML="TIE";}
